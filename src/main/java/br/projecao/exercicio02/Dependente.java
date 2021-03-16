75926f6fa01fe03625eb62674c318fd832b5963e
Devwarlt <nadio.pontes@redes.unb.br>
2021-03-15 22:54:54 -0300

    Configuração do template básico do projeto

---
+++ src/main/java/br/projecao/exercicio02/Dependente.java
@@ -0,0 +1,6 @@
+package br.projecao.exercicio02;
+
+public class Dependente {
+    private string nome;
+    private date date;
+}
---
+++ src/main/java/br/projecao/exercicio02/Socio.java
@@ -0,0 +1,8 @@
+package br.projecao.exercicio02;
+
+public class Socio {
+    private String nome;
+    private String endereço;
+    private int telefone;
+    private int datanascimento;
+}
